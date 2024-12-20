f5255c1ebcf1f708aa1326df81f7a37c6ec6bb38
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-12-20 22:58:35 +0100

    Make config module global.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
 import { UsersModule } from './users/application/users.module';
 import { APP_FILTER } from '@nestjs/core';
@@ -6,10 +6,16 @@ import { PrismaErrorFilter } from './database/prisma/filters/prisma-error-filter
 import { IamModule } from './core/iam/application/iam.module';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './config/app.config';
+import authenticationConfig from './core/iam/application/authentication/config/authentication.config';
+import prismaConfig from './database/prisma/config/prisma.config';
 
+@Global()
 @Module({
   imports: [
-    ConfigModule.forFeature(appConfig),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, authenticationConfig, prismaConfig],
+    }),
     CoreModule,
     IamModule,
     UsersModule,
