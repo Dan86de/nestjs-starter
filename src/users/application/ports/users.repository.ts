0f699812709e4e3df4720f768ac184f5a6316dd7
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-12-20 11:22:23 +0100

    Add endpoints for finding user and deleting user.

--- src/users/application/ports/users.repository.ts
+++ src/users/application/ports/users.repository.ts
@@ -2,4 +2,6 @@ import { User } from '../../domain/user';
 
 export abstract class UsersRepository {
   abstract findAll(): Promise<User[]>;
+  abstract findOne(id: string): Promise<User>;
+  abstract delete(id: string): Promise<void>;
 }
