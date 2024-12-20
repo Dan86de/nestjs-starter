d1793655ed6327e49caa6f3a9598f36ee21909c2
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-12-20 23:12:01 +0100

    Revert

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,15 @@ import { APP_FILTER } from '@nestjs/core';
 import { PrismaErrorFilter } from './database/prisma/filters/prisma-error-filter/prisma-error.filter';
 import { IamModule } from './core/iam/application/iam.module';
 import { ConfigModule } from '@nestjs/config';
-import appConfig from './config/app.config';
-import authenticationConfig from './core/iam/application/authentication/config/authentication.config';
-import prismaConfig from './database/prisma/config/prisma.config';
+import config, { configurationValidationSchema } from './config/app.config';
 
 @Global()
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, authenticationConfig, prismaConfig],
+      load: [config],
+      validationSchema: [configurationValidationSchema],
     }),
     CoreModule,
     IamModule,
