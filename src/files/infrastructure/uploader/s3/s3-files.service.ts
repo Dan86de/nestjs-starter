e01a7d2490841af0db20460fd7b2d0c7f6b27ea3
Daniel Noworyta <daniel.noworyta@gmail.com>
2024-12-22 19:36:10 +0100

    Fix public url for uploaded file.

--- src/files/infrastructure/uploader/s3/s3-files.service.ts
+++ src/files/infrastructure/uploader/s3/s3-files.service.ts
@@ -5,10 +5,14 @@ import {
 } from '@nestjs/common';
 import { File } from '../../../domain/file';
 import { FilesRepository } from '../../../application/ports/files.repository';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class S3FilesService {
-  constructor(private readonly fileRepository: FilesRepository) {}
+  constructor(
+    private readonly fileRepository: FilesRepository,
+    private readonly configService: ConfigService,
+  ) {}
 
   async create(file: Express.MulterS3.File): Promise<File> {
     if (!file) {
@@ -20,6 +24,10 @@ export class S3FilesService {
       });
     }
 
-    return await this.fileRepository.create({ path: file.location });
+    const filePublicUrl = this.configService.get('AWS_S3_PUBLIC_URL');
+    const bucketName = this.configService.get('AWS_S3_BUCKET_NAME');
+    const path = `${filePublicUrl}/${bucketName}/${file.key}`;
+
+    return await this.fileRepository.create({ path });
   }
 }
